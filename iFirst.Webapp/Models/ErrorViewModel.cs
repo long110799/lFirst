e29ad8ac62d8568fae01e65d1c1d0678c82da292
Hồ Phi Long <Long171A010063@st.vhu.edu.vn>
2020-10-26 10:10:43 +0700

    Ho Phi Long

---
+++ iFirst.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iFirst.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
