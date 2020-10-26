4b9144179f57e230389de10f0f2a47f17d804571
Hồ Phi Long <Long171A010063@st.vhu.edu.vn>
2020-10-26 10:26:47 +0700

    Create Solution

---
+++ Read.me/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Read.me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lFirst.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace lFirst.Data
+{
+    public class Class1
+    {
+    }
+}
